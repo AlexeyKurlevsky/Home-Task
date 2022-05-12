6d6ed4d0bd0798c4bfd3dd18b14991cb58c37880
Alexey Kurlevsky <kurlevsky@gmail.com>
2022-05-12 20:06:15 +0300

    Add cheking key in a dictionary

---
+++ task_dictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task_dictionary
+{
+    class task_dictionary
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> countries = new Dictionary<string, string>();
+
+            //first method how to add vaules in dict
+
+            countries.Add("uk", "United Kingdom");
+            countries.Add("rus", "Russia");
+            countries.Add("aus", "Australia");
+
+            //second mehtod how to add values in dict
+
+            countries["chn"] = "China";
+            countries["esp"] = "Spain";
+            string new_country = Console.ReadLine();
+
+            //validating an existing key
+
+            if (countries.ContainsKey(new_country))
+            {
+                Console.WriteLine("This key already exists");
+            }
+            else
+            {
+                Console.WriteLine("No such key exists");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
